ed0f17a954c9e4bf6b35a8aaea30f6e94164d90b
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-03-09 19:18:50 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./Components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Header />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
